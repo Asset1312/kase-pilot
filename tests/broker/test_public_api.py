d7f6a44f5cee3c7b04f0a44b70851a41553b89b8
Aset <asetsh@gmail.com>
2026-07-27 11:09:16 +0500

    test(broker): lock public package exports

---
+++ tests/broker/test_public_api.py
@@ -0,0 +1,77 @@
+"""Tests for the public surface of :mod:`kase_pilot.broker`."""
+
+import pytest
+
+from kase_pilot import broker
+from kase_pilot.broker import (
+    BrokerClient,
+    MarketService,
+    OrdersService,
+    PortfolioService,
+    ReportsService,
+)
+from kase_pilot.broker.client import BrokerClient as ModuleBrokerClient
+from kase_pilot.broker.market import MarketService as ModuleMarketService
+from kase_pilot.broker.orders import OrdersService as ModuleOrdersService
+from kase_pilot.broker.portfolio import PortfolioService as ModulePortfolioService
+from kase_pilot.broker.reports import ReportsService as ModuleReportsService
+
+
+def test_broker_package_imports_successfully() -> None:
+    assert broker is not None
+
+
+def test_public_classes_import_successfully() -> None:
+    assert all(
+        symbol is not None
+        for symbol in (
+            BrokerClient,
+            MarketService,
+            OrdersService,
+            PortfolioService,
+            ReportsService,
+        )
+    )
+
+
+def test_all_contains_exactly_current_public_exports() -> None:
+    assert set(broker.__all__) == {
+        "BrokerClient",
+        "MarketService",
+        "OrdersService",
+        "PortfolioService",
+        "ReportsService",
+    }
+    assert len(broker.__all__) == 5
+
+
+@pytest.mark.parametrize(
+    ("exported", "module_class"),
+    [
+        (BrokerClient, ModuleBrokerClient),
+        (MarketService, ModuleMarketService),
+        (OrdersService, ModuleOrdersService),
+        (PortfolioService, ModulePortfolioService),
+        (ReportsService, ModuleReportsService),
+    ],
+)
+def test_exported_symbol_is_source_module_class(
+    exported: type[object],
+    module_class: type[object],
+) -> None:
+    assert exported is module_class
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        "SecuritySession",
+        "AuthProvider",
+        "HttpTransport",
+        "JsonValue",
+        "RawPayload",
+    ],
+)
+def test_internal_symbols_are_not_exported(name: str) -> None:
+    assert name not in broker.__all__
+    assert not hasattr(broker, name)
